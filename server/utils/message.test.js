9811bbd4f54cc251d1936fdfbdcb3ffe16b6bb44
ldevernay <boblehamster@yahoo.fr>
2017-08-28 16:40:29 +0200

    test message generation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -6,7 +6,9 @@ describe('generate message', () => {
   it('should generate the correct message object', () => {
     let from = 'Bob';
     let text = 'Testing';
+    
     let res = generateMessage(from, text);
+
     expect(res.from).toEqual(from);
     expect(res.text).toEqual(text);
     expect(res.createdAt).toBeA('number');
