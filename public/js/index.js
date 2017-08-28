4e5843dfbf834f7faabfc41c63a8716cd2e16948
ldevernay <boblehamster@yahoo.fr>
2017-08-28 15:44:27 +0200

    wire server and front

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,12 @@
 let socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Bob',
-    text: 'Hey this is nice!'
-  })
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
-socket.on(`newMnewMessage`, message);
+socket.on(`newMessage`, function(message) {
+  console.log('newMessage', message);
 });
