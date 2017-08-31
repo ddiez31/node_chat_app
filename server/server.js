11d893f931dfbdef07aac6292ce78ebe44da2315
ldevernay <boblehamster@yahoo.fr>
2017-08-31 15:08:07 +0200

    send notifications upon entering chatroom

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const Push = require('push.js');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
@@ -37,7 +36,6 @@ io.on('connection', (socket) => {
 
     socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined the room.`));
 
-        Push.create('Welcome!');
     callback();
   });
 
